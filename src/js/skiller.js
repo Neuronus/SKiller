d9c0067e3199e9c00da18f2c194acbaf659a5751
Neuronus <piotr@interacti-labs.pl>
2015-03-28 22:04:35 +0100

    base code of plugin

--- src/js/skiller.js
+++ src/js/skiller.js
@@ -8,3 +8,27 @@ $().ready(function(){
 	var $skillers = $(document).find('.skiller-container');
 	
 });
+
+
+(function($) {
+    $.fn.SKiller = function( options ) {
+
+        // default settings
+        var settings = $.extend({
+        	type			: null,
+        	speed			: 500,
+            color			: null,
+            innerBorder		: null,
+            outerBorder		: null,
+            complete		: null            
+        }, options);
+
+        return this.each( function() {
+			
+			if ( $.isFunction( settings.complete ) ) {
+        		settings.complete.call( this );
+    		} 
+        });
+
+    }
+}(jQuery));
