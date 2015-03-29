2e1170496b66d255c32b304bb993fedc63edec0a
Neuronus <piotr@interacti-labs.pl>
2015-03-29 01:00:46 +0100

    some stuff
    base HTML stuff. adding tags, classes and start animation added

--- src/js/skiller.js
+++ src/js/skiller.js
@@ -4,11 +4,6 @@
  * 
  */
 
-$().ready(function(){
-	var $skillers = $(document).find('.skiller-container');
-	
-});
-
 
 (function($) {
 	"use strict";
@@ -26,9 +21,27 @@ $().ready(function(){
 		}, options);
 	
 		return this.each( function() {
-		
-		
-		
+			console.log($(this));
+			
+			var $skillBars = $(this).find('> div'); 
+			
+			$skillBars.each(function(){
+				var percent = $(this).attr('data-lvl');
+				$(this).addClass('skill');
+				$(this).find(':first')
+					.after('<div class="skill-bar-border"><div class="skill-bar"></div></div>')
+				var $skillBar = $(this).find('.skill-bar'); 
+				var $barBorder = $(this).find('.skill-bar-border'); 
+				var pr = (($barBorder.width())*percent)/100;
+				
+				if (settings.type == 1) 
+				$skillBar.html(percent+'%')
+				$skillBar.animate({'width': pr+'px'}, settings.speed);
+						
+			});		
+			
+			
+			
 			// Execute user function after animation complete
 			if ( $.isFunction( settings.complete ) ) {
 				settings.complete.call( this );
@@ -37,3 +50,11 @@ $().ready(function(){
 
     }
 }(jQuery));
+
+
+
+
+$().ready(function(){
+	var $skillers = $(document).find('.skiller-container');
+	$('.skills').SKiller({type: 1, speed: 800});
+});
